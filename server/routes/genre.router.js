d320526f9c5363477d0a1078eb490488752425f6
Michael Huso <husomichael@gmail.com>
2021-12-12 10:32:24 -0600

    setup genreRouter GET request

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -3,8 +3,14 @@ const router = express.Router();
 const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
-  // Add query to get all genres
-  res.sendStatus(500)
+  const queryText = `SELECT "name" FROM "genres";`;
+  pool.query(queryText)
+    .then((dbRes) =>{
+      res.send(dbRes.rows);
+    }).catch((dbErr) =>{
+      console.log('Error in genres GET:', dbErr);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
